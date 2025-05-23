d125af24b54eedf8e2ed96fe6687b638d2401b84
pyrit33 <deloneycheyenne@gmail.com>
2025-05-22 21:52:10 -0500

    Update RoomAdventure.java
    fixed bug for damageables

--- RoomAdventure.java
+++ RoomAdventure.java
@@ -146,6 +146,7 @@ public class RoomAdventure { // Main class containing game logic
             "It's a desk, there is a key on it."
         };
         String[] room1Grabbables = {"key"}; // Items you can take in Room 1
+        String[] room1Damageables = {null}; // Items that can damage you
         room1.setExitDirections(room1ExitDirections); // Set exits
         room1.setExitDestinations(room1ExitDestinations); // Set exit destinations
         room1.setItems(room1Items); // Set visible items
